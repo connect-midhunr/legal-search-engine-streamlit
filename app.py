76c77809b0f29ce7267171b8ba0607fff9800f3b
midhun.r <midhun.r@saasvaap.com>
2024-06-05 21:27:20 +0530

    Added feature to stream output

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from document_QnA import create_text_chunks, create_vector_store, create_chat_co
 import chromadb
 import os
 import json
+import time
+import numpy as np
+import pandas as pd
 
 def load_config(filename):
     with open(filename, 'r') as f:
@@ -29,92 +32,124 @@ list_of_case_titles = [dictionary['case_title'] for dictionary in all_documents[
 list_of_doc_ids = all_documents['ids']
 dict_of_options = {list_of_case_titles[num]:list_of_doc_ids[num] for num in range(len(list_of_case_titles))}
 
+# function to stream response message from the bot
+def stream_data(message):
+    try:
+        for word in message.split(" "):
+            yield word + " "
+            time.sleep(0.02)
+
+    except Exception as e:
+        print(f"Exception occured in stream_data: {e}")
+        return None
+
 # function to generate user-to-bot chat
 def generate_chat_from_user_question(user_question):
-    if st.session_state.conversation:
-        response = st.session_state.conversation({'question': user_question})
-        st.session_state.chat_history = response['chat_history']
+    try:
+        if st.session_state.conversation:
+            response = st.session_state.conversation({'question': user_question + "Provide the answer in at least 100 words."})
+            st.session_state.chat_history = response['chat_history']
 
-        for i, message in enumerate(st.session_state.chat_history):
-            if i % 2 == 0:
-                st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-            else:
-                st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-    else:
-        st.write(alert_bot_template.replace("{{MSG}}", "Click the 'Process' button before starting the session."), unsafe_allow_html=True)
+            for i, message in enumerate(st.session_state.chat_history):
+                if i % 2 == 0:
+                    with st.chat_message("user"):
+                        st.write(message.content.replace("Provide the answer in at least 100 words.", ""))
+                else:
+                    with st.chat_message("assistant"):
+                        if i == len(st.session_state.chat_history) - 1:
+                            st.write_stream(stream_data(message.content))
+                        else:
+                            st.write(message.content)
+
+        else:
+            st.write(alert_bot_template.replace("{{MSG}}", "Click the 'Process' button before starting the session."), unsafe_allow_html=True)
+
+    except Exception as e:
+        print(f"Exception occured in generate_chat_from_user_question: {e}")
+        return None
             
 # function to retrieve document title and text using document id
 def get_doc_title_and_text(doc_id):
-    result = doc_collection.get(ids=[doc_id])
-    doc_title = result['metadatas'][0]['case_title']
-    doc_text = result['documents'][0]
-    return doc_title, doc_text
+    try:
+        result = doc_collection.get(ids=[doc_id])
+        doc_title = result['metadatas'][0]['case_title']
+        doc_text = result['documents'][0]
+        return doc_title, doc_text
+
+    except Exception as e:
+        print(f"Exception occured in get_doc_title_and_text: {e}")
+        return None
 
 if __name__ == '__main__':
-    st.set_page_config(page_title="Legal Docs Search & QnA", page_icon=":robot:")
-    st.write(css, unsafe_allow_html=True)
+    try:
+        st.set_page_config(page_title="Legal Docs Search & QnA", page_icon=":robot:")
+        st.write(css, unsafe_allow_html=True)
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+        if "conversation" not in st.session_state:
+            st.session_state.conversation = None
+        if "chat_history" not in st.session_state:
+            st.session_state.chat_history = None
 
-    col1, col2, col3 = st.columns(3)
-    with col2:
-        st.image(f'{current_directory}/images/collabll-logo.png')
+        col1, col2, col3 = st.columns(3)
+        with col2:
+            st.image(f'{current_directory}/images/collabll-logo.png')
 
-    doc_id = st.query_params.get('docid')
-    # print('doc-id:', doc_id)
+        doc_id = st.query_params.get('docid')
+        # print('doc-id:', doc_id)
 
-    # selected_tab = option_menu(
-    #     menu_title=None,
-    #     options=["Search", "QnA"],
-    #     icons=["search", "robot"],
-    #     menu_icon="cast",
-    #     default_index=0,
-    #     orientation="horizontal",
-    #     styles={"nav-link-selected": {"background-color": "#2986cc"}}
-    # )
+        # selected_tab = option_menu(
+        #     menu_title=None,
+        #     options=["Search", "QnA"],
+        #     icons=["search", "robot"],
+        #     menu_icon="cast",
+        #     default_index=0,
+        #     orientation="horizontal",
+        #     styles={"nav-link-selected": {"background-color": "#2986cc"}}
+        # )
 
-    if doc_id:
-        st.write(header_template.replace("{{MSG}}", "Legal Docs QnA"), unsafe_allow_html=True)
-        st.write("This tab demonstrates the 'Question and Answer' part of the application.")
+        if doc_id:
+            st.write(header_template.replace("{{MSG}}", "Legal Docs QnA"), unsafe_allow_html=True)
+            st.write("This tab demonstrates the 'Question and Answer' part of the application.")
 
-        doc_title, doc_text = get_doc_title_and_text(doc_id)
+            doc_title, doc_text = get_doc_title_and_text(doc_id)
 
-        st.write(f'The case you have selected:  \n**{doc_title}**')
+            st.write(f'The case you have selected:  \n**{doc_title}**')
 
-        # doc_id = dict_of_options[st.selectbox("Select a case", list(dict_of_options.keys()))]
-        # if st.button("Process")
-        if not st.session_state.conversation:
-            with st.spinner("Processing..."):
-                text_chunks = create_text_chunks(doc_text)
-                vector_store = create_vector_store(text_chunks)
-                st.session_state.conversation = create_chat_conversation(vector_store)
+            # doc_id = dict_of_options[st.selectbox("Select a case", list(dict_of_options.keys()))]
+            # if st.button("Process")
+            if not st.session_state.conversation:
+                with st.spinner("Processing..."):
+                    text_chunks = create_text_chunks(doc_text)
+                    vector_store = create_vector_store(text_chunks)
+                    st.session_state.conversation = create_chat_conversation(vector_store)
 
-        user_question = st.text_input("Ask a question about your documents:")
-        if user_question:
-            generate_chat_from_user_question(user_question)
+            user_question = st.chat_input("Ask a question about your document")
+            if user_question:
+                with st.spinner("Generating response..."):
+                    generate_chat_from_user_question(user_question)
 
-    else:
-        st.write(header_template.replace("{{MSG}}", "Legal Docs Search"), unsafe_allow_html=True)
-        st.write("This tab demonstrates the 'Search and Find Documents' part of the application.")
+        else:
+            st.write(header_template.replace("{{MSG}}", "Legal Docs Search"), unsafe_allow_html=True)
+            st.write("This tab demonstrates the 'Search and Find Documents' part of the application.")
 
-        search_document = st.text_input("Search and find relevant documents:")
-        if search_document:
-            with st.spinner("Loading..."):
-                result = doc_collection.query(query_texts=search_document, n_results=10)
-                # st.write(result)
+            search_document = st.text_input("Search and find relevant documents:")
+            if search_document:
+                with st.spinner("Loading..."):
+                    result = doc_collection.query(query_texts=search_document, n_results=10)
+                    # st.write(result)
 
-                for num in range(len(result['ids'][0])):
-                    metadata = result['metadatas'][0][num]
-                    doc_id = result['ids'][0][num]
-                    result_template = search_result_template.replace("{{CASE_TITLE}}", metadata['case_title'])
-                    result_template = result_template.replace("{{CASE_TYPE}}", metadata['case_type'])
-                    result_template = result_template.replace("{{CNR_NUMBER}}", metadata['cnr_num'])
-                    result_template = result_template.replace("{{INTERIM_ORDERS_URL}}", generate_interim_orders_info(eval(metadata['list_of_interim_order_urls'])))
-                    result_template = result_template.replace("{{JUDGEMENT_URL}}", generate_judgement_info(metadata['judgement_url']))
-                    result_template = result_template.replace("{{START_QNA_URL}}", doc_id)
-                    # print(result_template)
-                    st.write(result_template, unsafe_allow_html=True)
+                    for num in range(len(result['ids'][0])):
+                        metadata = result['metadatas'][0][num]
+                        doc_id = result['ids'][0][num]
+                        result_template = search_result_template.replace("{{CASE_TITLE}}", metadata['case_title'])
+                        result_template = result_template.replace("{{CASE_TYPE}}", metadata['case_type'])
+                        result_template = result_template.replace("{{CNR_NUMBER}}", metadata['cnr_num'])
+                        result_template = result_template.replace("{{INTERIM_ORDERS_URL}}", generate_interim_orders_info(eval(metadata['list_of_interim_order_urls'])))
+                        result_template = result_template.replace("{{JUDGEMENT_URL}}", generate_judgement_info(metadata['judgement_url']))
+                        result_template = result_template.replace("{{START_QNA_URL}}", doc_id)
+                        # print(result_template)
+                        st.write(result_template, unsafe_allow_html=True)
+
+    except Exception as e:
+        print(f"Exception occured in main: {e}")
             

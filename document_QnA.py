d5e533d43231893fe3445c947ef2033c87bbce5b
midhun.r <midhun.r@saasvaap.com>
2024-06-05 21:28:03 +0530

    Updated packages and tried out gemini pro

--- document_QnA.py
+++ document_QnA.py
@@ -6,15 +6,19 @@ import json
 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.embeddings.huggingface import HuggingFaceBgeEmbeddings
-from langchain.vectorstores.faiss import FAISS
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain_community.llms.ollama import Ollama
+from langchain_community.llms.huggingface_hub import HuggingFaceHub
+from langchain_google_genai import ChatGoogleGenerativeAI
+import google.generativeai as genai
 
 load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
 huggingfacehub_api_token = os.getenv('HUGGINGFACEHUB_API_TOKEN')
+google_api_key = os.getenv('GOOGLE_API_KEY')
 
 def load_config(filename):
     with open(filename, 'r') as f:
@@ -27,55 +31,88 @@ huggingfacehub_embedding_model = config['embedding_models']['faiss_embedding_mod
 openai_model = config['llm_models']['openai_model']
 ollama_model = config['llm_models']['ollama_model']
 
+genai.configure(api_key=google_api_key)
+
 # print(f"OpenAI key: {openai_api_key}")
 
 # function to identify all line endings in a string.
 def get_line_endings(text):
-  line_endings = []
-  for i, char in enumerate(text):
-    if char in ("\n", "\r") and char != "\n\n":
-      line_endings.append(text[i-1])
-  return line_endings
+  try:
+    line_endings = []
+    for i, char in enumerate(text):
+      if char in ("\n", "\r") and char != "\n\n":
+        line_endings.append(text[i-1])
+    return line_endings
+
+  except Exception as e:
+      print(f"Exception occured in get_line_endings: {e}")
+      return None
 
 # function to clean text
 def clean_text(text):
-  cleaned_text = ""
-  # Iterate through each line in the text
-  for line in text.splitlines():
-    # Check if the line ends with a space or hyphen
-    if line.endswith(""):
-      # If it does, concatenate with the next line without a newline
-      cleaned_text += line[:-1]
-    else:
-      # If not, add a newline and the current line
-      cleaned_text += line + "\n"
-  return cleaned_text
+  try:
+    cleaned_text = ""
+    # Iterate through each line in the text
+    for line in text.splitlines():
+      # Check if the line ends with a space or hyphen
+      if line.endswith(""):
+        # If it does, concatenate with the next line without a newline
+        cleaned_text += line[:-1]
+      else:
+        # If not, add a newline and the current line
+        cleaned_text += line + "\n"
+    return cleaned_text
+
+  except Exception as e:
+      print(f"Exception occured in clean_text: {e}")
+      return None
 
 # function to create text chunks
 def create_text_chunks(text):
+  try:
     chunks = text.split(25*'-')
     # for chunk in chunks:
     #   print(chunk)
     #   print(25*'*')
     return text.split(25*'-')
 
+  except Exception as e:
+    print(f"Exception occured in create_text_chunks: {e}")
+    return None
+
 # function to create vector store
 def create_vector_store(text_chunks):
+  try:
+    # import pprint
+    # for model in genai.list_models():
+    #     pprint.pprint(model)
+
     # embedding = OpenAIEmbeddings()
     embedding = HuggingFaceBgeEmbeddings(model_name=huggingfacehub_embedding_model, model_kwargs={"device": "cpu"}, encode_kwargs={"normalize_embeddings": True})
     vector_store = FAISS.from_texts(texts=text_chunks, embedding=embedding)
     return vector_store
 
+  except Exception as e:
+    print(f"Exception occured in create_vector_store: {e}")
+    return None
+
 # function to create conversation of the chatbot
 def create_chat_conversation(vector_store):
+  try:
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     # llm = ChatOpenAI(model_name=openai_model)
-    llm = Ollama(model=ollama_model, temperature=0.6)
+    # llm = Ollama(model=ollama_model, temperature=0.6)
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
+    # llm = HuggingFaceHub(repo_id="microsoft/Phi-3-small-128k-instruct", huggingfacehub_api_token=huggingfacehub_api_token)
     conversation_chain = ConversationalRetrievalChain.from_llm(
           llm=llm,
           retriever=vector_store.as_retriever(search_type = "mmr"),
           memory=memory)
     return conversation_chain
+
+  except Exception as e:
+    print(f"Exception occured in create_chat_conversation: {e}")
+    return None
 
 if __name__ == '__main__':
     client = chromadb.PersistentClient(path='data')
